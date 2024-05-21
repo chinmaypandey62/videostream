8fe1f5968a2e61b81eb749898ca3a78003384360
chinmaypandey62 <chinmaypandey62@gmail.com>
2024-05-22 00:28:05 +0530

    Created user controller

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 //used async function because the DB might take some time to respond
 const connectDB = async () => {
-    //used try-catch because the connection might get some error
+    //I used try-catch because the connection might get some error
     try {
         //we need to provide the path for connection of DB which is (MONGODB_URI/DB_NAME)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
